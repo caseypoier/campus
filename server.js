021095ece2c31962a0bef12f38c0bf4334b46649
Casey Poier <caseypoier@Evos-MacBook-Pro.local>
2020-12-04 18:13:53 -0800

    updating

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.get ('/',  function(req, res){
     res.render('main');
 });
 
-app.listen(3000, function(){
-    console.log('server started on port 3000...');
-});
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
